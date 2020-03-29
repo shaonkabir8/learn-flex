967d79fe3313c84697e7a2ebb84cb1bdadf37094
Shaonkabir <shaonkabir98@gmail.com>
2020-03-29 22:59:58 +0600

    refactor(gulp): add gulp watch task for watching on everytime css changed!!

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
-const { task, src, dest } = require('gulp');
+const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-task('styles', function(){
+// gulp task for geneating autoprefix on css
+gulp.task('styles', function() {
     
-    return src('style.css')
+    return gulp.src('style.css')
         .pipe(autoprefixer())
-        .pipe(dest('build'))
+        .pipe(gulp.dest('build'))
 
 })
+
+
+// gulp task for watching on everytime css changed!!
+gulp.task('watch', function() {
+    gulp.watch('style.css', gulp.series('styles'));
+});
+
+// I just have to waste a lot of times to making this `watch` task completely successfull.
+// found solution on here: https://stackoverflow.com/questions/39665773/gulp-error-watch-task-has-to-be-a-function
